4c9948e66995836dab2475d537c6884dbd1c21a4
Andreea <grayscale130@gmail.com>
2025-02-18 10:52:49 +0200

    a)

--- RESTusleaAndreea732Aufgabe1/src/Main.java
+++ RESTusleaAndreea732Aufgabe1/src/Main.java
@@ -1,10 +1,51 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws Exception {
+
+        //a)
+        File file = new File("ninja_events.json");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        Map<String, String> currentLog = new HashMap<>();
+        List<Map<String, String>> logsList = new ArrayList<>();
+        String line = bufferedReader.readLine();
+        while(line != null)
+        {
+            if(line.contains("[") || line.contains("]") || line.contains("{")) {
+
+                // mergem doar la urmatoarea linie
+                line = bufferedReader.readLine();
+                continue;
+            }
+
+            if(line.contains("}")) {
+                logsList.add(currentLog);
+
+                currentLog = new HashMap<>();
+
+                line = bufferedReader.readLine();
+                continue;
+            }
+
+            line = line.replace("\"", "");
+
+            String[] data = line.split("[:,]");
+
+            data[0] = data[0].trim();
+            data[1] = data[1].trim();
+            currentLog.put(data[0], data[1]);
+            line = bufferedReader.readLine();
+        }
 
     }
 }
